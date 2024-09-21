8fdf0531ededcb0aab8d4f87f5cbd01dcb8b24a4
suvo <suvodatta72@gmail.com>
2024-09-21 23:11:10 +0600

    Basic setUp done

---
+++ js/utility.js
@@ -0,0 +1,19 @@
+function $(id){
+    return document.getElementById(id);
+}
+
+function $value(id){
+    return document.getElementById(id).value;
+}
+
+function $text(id){
+    return document.getElementById(id).innerText;
+}
+
+function $element(id){
+    return document.getElementById(id).innerHTML;
+}
+
+function $togle(id){
+    return document.getElementById(id)
+}
